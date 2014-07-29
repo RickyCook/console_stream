caccf338a218d5ecb323eeb9468fdcc0b25984db
Ricky Cook <ricky@infoxchange.net.au>
2014-07-30 06:19:39 +1000

    First cmd runner

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+import asyncio
+import os
+import subprocess
+import sys
+
+import websockets
+
 from flask import Flask, render_template
 app = Flask(__name__)
 
@@ -5,5 +12,26 @@ app = Flask(__name__)
 def main():
     return render_template('index.html')
 
+@asyncio.coroutine
+def command(websocket, path):
+    cmd = yield from websocket.recv()
+    print("CMD< %s" % cmd)
+
+    process = subprocess.Popen(('/bin/sh', '-c', cmd),
+                               stdout=subprocess.PIPE)
+
+    sys.stdout.write("CMD> ")
+    for byte in iter(lambda: process.stdout.read(1), b''):
+        sys.stdout.write(byte.decode('utf-8'))
+        yield from websocket.send(byte)
+        if byte == b"\n":
+            sys.stdout.write("CMD> ")
+
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    pid = os.fork()
+    if pid:
+        app.run(debug=True, host='0.0.0.0')
+    else:
+        socket_server = websockets.serve(command, '0.0.0.0', 5001)
+        asyncio.get_event_loop().run_until_complete(socket_server)
+        asyncio.get_event_loop().run_forever()
--- static/js/main.js
+++ static/js/main.js
@@ -1,13 +1,30 @@
 $(document).ready(function() {
 	var console_ = $('#console'),
 	    command = $('#command'),
-	    run = $('#run')
+	    run = $('#run'),
+	    host = document.location.hostname
 
+	function command_output(part) {
+		str = String.fromCharCode.apply(null, new Uint8Array(part))
+		console.log(str)
+		console_.append(str)
+	}
 	function send_command(command) {
-		console_.text('$ ' + command + ' # not run')
+		var connection = new WebSocket('ws://' + host + ':5001')
+		connection.binaryType = "arraybuffer"
+		connection.onopen = function() {
+			connection.send(command)
+			console_.text("$ " + command + "\n")
+		}
+		connection.onmessage = function(ev) {
+			console.log(ev)
+			command_output(ev.data)
+		}
+		connection.onerror = function(err) { alert("Error: " + err) }
+		connection.onclose = function() { alert("Done") }
 	}
 	run.click(function() {
 		send_command(command.val())
 	})
-	console_.html('ready...')
+	console_.html("ready...")
 })
